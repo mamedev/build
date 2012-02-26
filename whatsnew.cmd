8beb29e96e3c58b47f2d9dbfc7176688590b75b1
Angelo Salese <angelosa@users.noreply.github.com>
2012-02-26 19:44:41 +0000

    Whatsnew for 0.145u3

--- whatsnew.cmd
+++ whatsnew.cmd
@@ -1,5 +1,5 @@
 @echo off
-set LASTREV=r15453
+set LASTREV=r15539
 for /f "usebackq tokens=1-2 delims=:" %%i in (`svn info svn://mamedev.org/mame/tags`) do if /i "%%i"=="last changed rev" set LASTREV=%%j
 echo Last rev = %LASTREV%
 svn log svn://mamedev.org/mame/trunk -v -r %LASTREV%:HEAD
