fa24c3c11792716501dc8fcb934bc94f74109b4b
Miodrag Milanovic <mmicko@gmail.com>
2015-09-04 11:50:13 +0200

    update of whatsnew.cmd (nw)

--- whatsnew.cmd
+++ whatsnew.cmd
@@ -1,6 +1,4 @@
 @echo off
-for /f %%i in ('git describe --tags --abbrev=0') do set LASTREV=%%i
-
 rem for /f "usebackq tokens=1-2 delims=:" %%i in (`svn info https://github.com/mamedev/mame/tags`) do if /i "%%i"=="last changed rev" set LASTREV=%%j
-echo Last = %LASTREV%
-rem svn log https://github.com/mamedev/mame/trunk -v -r %LASTREV%:HEAD
+set LASTREV=r248552
+svn log https://github.com/mamedev/mame/trunk -v -r %LASTREV%:HEAD
