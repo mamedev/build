83d82b90d7b3fb3721703a27d78cf52465fb5947
Miodrag Milanovic <mmicko@gmail.com>
2016-02-22 12:10:52 +0100

    Add items in order from oldest to latest (nw)

--- whatsnew.cmd
+++ whatsnew.cmd
@@ -1,4 +1,4 @@
 @echo off
 for /f "delims=" %%a in ('git -C ../mame describe --tags --abbrev^=0') do @set LAST_TAG=%%a
-git -C ../mame log %LAST_TAG%..HEAD > temp.log
+git -C ../mame log --reverse %LAST_TAG%..HEAD > temp.log
 makewn temp.log > whatsnew.txt
