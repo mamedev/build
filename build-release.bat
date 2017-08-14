3d4541e5b278bc74c51f2b796179b73ddb7af8ed
Vas Crabb <vas@vastheman.com>
2017-08-14 19:29:32 +1000

    move a bunch of stuff out of here into a makefile in MAME repo

--- build-release.bat
+++ build-release.bat
@@ -1,7 +1,9 @@
 @echo Building 32-bit release version....
  
 make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=0 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 -j9
+make -f dist.mak PTR64=0
 
 @echo Building 64-bit release version....
  
 make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 -j9
+make -f dist.mak PTR64=1
--- release.bat
+++ release.bat
@@ -42,47 +42,22 @@ echo Put build number as parameter
 goto :eof
 
 :copyfiles
-copy %1\%3.exe %2\. /Y > nul
 copy %1\%3.sym %2\. /Y > nul
 copy ..\build\whatsnew\whatsnew_%5.txt %2\whatsnew.txt /Y > nul
-copy %1\chdman.exe %2\. /Y > nul
-copy %1\ldverify.exe %2\. /Y > nul
-copy %1\ldresample.exe %2\. /Y > nul
-copy %1\romcmp.exe %2\. /Y > nul
-copy %1\jedutil.exe %2\. /Y > nul
-copy %1\unidasm.exe %2\. /Y > nul
-copy %1\nltool.exe  %2\. /Y > nul
-copy %1\nlwav.exe  %2\. /Y > nul
-copy %1\castool.exe %2\. /Y > nul
-copy %1\floptool.exe %2\. /Y > nul 
-copy %1\imgtool.exe %2\. /Y > nul 
 
 7za x ..\build\mamedirs.zip -o%2 >nul
 
-mkdir %2\docs 2> nul
-xcopy docs\* %2\docs /s /i /y > nul
-mkdir %2\hash 2> nul
-xcopy hash\* %2\hash /s /i /y > nul 
-mkdir %2\hlsl 2> nul
-xcopy hlsl\* %2\hlsl /s /i /y > nul
-mkdir %2\nl_examples 2> nul
-xcopy nl_examples\* %2\nl_examples /s /i /y > nul
-mkdir %2\samples 2> nul
-xcopy samples\* %2\samples /s /i /y > nul
 mkdir %2\artwork 2> nul
 xcopy artwork\* %2\artwork /s /i /y > nul
 mkdir %2\bgfx 2> nul
 xcopy bgfx\* %2\bgfx /s /i /y > nul
+mkdir %2\hlsl 2> nul
+xcopy hlsl\* %2\hlsl /s /i /y > nul
 mkdir %2\plugins 2> nul
 xcopy plugins\* %2\plugins /s /i /y > nul
-mkdir %2\language 2> nul
-xcopy language\*.mo %2\language /s /i /y > nul
-mkdir %2\ini\presets 2> nul
-xcopy ini\presets\* %2\ini\presets /s /i /y > nul
+mkdir %2\samples 2> nul
+xcopy samples\* %2\samples /s /i /y > nul
 
-copy uismall.bdf %2\. /Y > nul 
-
-strip %2\*.exe
 echo Packing %4
 pushd %2
 7za a -mx=9 -y -r -t7z -sfx7z.sfx ..\..\..\%4 >nul
