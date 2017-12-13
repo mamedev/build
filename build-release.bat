41eddfcdfbd6e83222770603db2f61273f444514
Vas Crabb <vas@vastheman.com>
2017-12-13 22:09:09 +1100

    require SSE2 for 32-bit Windows binary package

--- build-release.bat
+++ build-release.bat
@@ -1,6 +1,6 @@
 @echo Building 32-bit release version....
  
-make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=0 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 -j9
+make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=0 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 -j9 ARCHOPTS="-msse2 -mfpmath=sse"
 make -f dist.mak PTR64=0
 
 @echo Building 64-bit release version....
