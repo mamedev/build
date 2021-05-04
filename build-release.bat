e002618b66773ae0bfdcf0cf4274196442f35880
Vas Crabb <vas@vastheman.com>
2021-05-05 05:29:37 +1000

    Add -fomit-frame-pointer to 64-bit build options - it works around MT07858.

--- build-release.bat
+++ build-release.bat
@@ -5,5 +5,5 @@ make -f dist.mak PTR64=0
 
 @echo Building 64-bit release version....
  
-make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 -j9 ARCHOPTS="-fuse-ld=lld"
+make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 -j9 ARCHOPTS="-fomit-frame-pointer -fuse-ld=lld" OVERRIDE_AR=llvm-ar
 make -f dist.mak PTR64=1
