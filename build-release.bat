51291bc175b458adcf520d87b78e1e14e222ab78
Vas Crabb <vas@vastheman.com>
2026-05-19 18:54:30 +1000

    Windows x64 releases will be built with clang, Windows x86 builds are no longer practical.

--- build-release.bat
+++ build-release.bat
@@ -1,14 +1,9 @@
-@echo Building x86 release version....
-
-make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=0 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 -j9 ARCHOPTS="-fuse-ld=lld -msse2 -mfpmath=sse"
-make -f dist.mak PTR64=0
-
 @echo Building x64 release version....
 
-make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 -j9 ARCHOPTS="-msahf -mcx16 -mpopcnt -msse4.2 -fomit-frame-pointer -fuse-ld=lld" OVERRIDE_AR=llvm-ar
+make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 OVERRIDE_CC=clang OVERRIDE_CXX=clang++ OVERRIDE_AR=llvm-ar -j9 ARCHOPTS="-fuse-ld=lld -march=x86-64-v2 -fomit-frame-pointer"
 make -f dist.mak PTR64=1
 
 @echo Building arm64 release version....
 
-make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 -j9 ARCHOPTS="-march=armv8.2-a -fuse-ld=lld" OVERRIDE_CC=clang OVERRIDE_CXX=clang++ OVERRIDE_AR=llvm-ar
+make TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 OVERRIDE_CC=clang OVERRIDE_CXX=clang++ OVERRIDE_AR=llvm-ar -j9 ARCHOPTS="-fuse-ld=lld -march=armv8.2-a"
 make -f dist.mak PTR64=1
